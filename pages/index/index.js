33d254a1998ce39f52aa94f02311dee9c1393ec2
李彦 <liyan@hualala.com>
2017-08-10 10:56:51 +0800

    重新获取

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,10 @@
 var app = getApp()
 
 function getPickerHandler(name) {
-  return function(e) {
+  return function (e) {
     this.setData({
-      [name]: e.detail.value
+      [name]: e.detail.value,
+      start: 0,
     })
   }
 }
@@ -13,7 +14,7 @@ function getPickerHandler(name) {
 Page({
   data: {
     movies: [],
-        tags: ["不限", "爱情", "喜剧", "动画", "剧情", "科幻", "动作", "经典", "悬疑", "青春", "电视剧", "犯罪", "惊悚", "文艺", "搞笑", "纪录片", "励志", "恐怖", "战争", "短片", "黑色幽默", "魔幻", "传记", "情色", "感人", "暴力", "动画短片", "家庭", "音乐", "童年", "浪漫", "黑帮", "女性", "同志", "史诗", "童话", "烂片", "cult"],
+    tags: ["不限", "爱情", "喜剧", "动画", "剧情", "科幻", "动作", "经典", "悬疑", "青春", "电视剧", "犯罪", "惊悚", "文艺", "搞笑", "纪录片", "励志", "恐怖", "战争", "短片", "黑色幽默", "魔幻", "传记", "情色", "感人", "暴力", "动画短片", "家庭", "音乐", "童年", "浪漫", "黑帮", "女性", "同志", "史诗", "童话", "烂片", "cult"],
     imgs: [],
     tagIndex: 0,
     mirIndex: 8,
@@ -23,8 +24,8 @@ Page({
     start: 0,
     // stopload: false,
     // loading: false,
-    order: [{"name": "rating", "sub": "评分"},
-            {"name": "rated", "sub": "人数"}],
+    order: [{ "name": "rating", "sub": "评分" },
+    { "name": "rated", "sub": "人数" }],
     orderIndex: 0,
     // showingmovie: null,
     rs: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
@@ -37,7 +38,7 @@ Page({
   bindMirnChange: getPickerHandler("mirnIndex"),
   bindMarnChange: getPickerHandler("marnIndex"),
   bindOrderChange: getPickerHandler("orderIndex"),
-  getMovies: function() {
+  getMovies: function () {
     var tag
     if (this.data.tagIndex == 0) {
       tag = ""
@@ -45,19 +46,19 @@ Page({
       tag = this.data.tags[this.data.tagIndex]
     }
     var url = "https://imliyan.com/watchmovie/"
-                + "tag=" + tag
-                + "/mir=" + this.data.mirIndex
-                + "/mar=" + this.data.marIndex
-                + "/mirn=" + this.data.ns[this.data.mirnIndex]
-                + "/marn=" + this.data.ns[this.data.marnIndex]
-                + "/od=" + this.data.order[this.data.orderIndex].name
-                + "/start=" + this.data.start + "/";
+      + "tag=" + tag
+      + "/mir=" + this.data.mirIndex
+      + "/mar=" + this.data.marIndex
+      + "/mirn=" + this.data.ns[this.data.mirnIndex]
+      + "/marn=" + this.data.ns[this.data.marnIndex]
+      + "/od=" + this.data.order[this.data.orderIndex].name
+      + "/start=" + this.data.start + "/";
     this.data.start += 20
     var preMovies = this.data.movies
     var that = this
     wx.request({
       url: url,
-      success: function(res) {
+      success: function (res) {
         that.setData({
           movies: preMovies.concat(res.data)
         })
@@ -65,6 +66,12 @@ Page({
     })
   },
   onLoad: function () {
+    this.getMovies()
+  },
+  reGet: function () {
+    this.setData({
+      movies: [],
+    })
     this.getMovies()
   },
   onReachBottom: function () {
